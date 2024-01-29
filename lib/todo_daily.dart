ca15daa92ecc821b7f402da9c09ba8783e32a510
lighteko <huijunyi0212@naver.com>
2024-01-30 02:04:24 +0900

    .

--- lib/todo_daily.dart
+++ lib/todo_daily.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class ToDoDaily extends StatefulWidget {
@@ -32,20 +33,38 @@ class _ToDoDailyState extends State<ToDoDaily> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          backgroundColor: const Color(0xff9B69DA),
+          elevation: 10,
+          shadowColor: Colors.black.withOpacity(0.5),
+          leading: IconButton(
+            icon: const Icon(
+              Icons.logout_outlined,
+              color: Colors.white,
+            ),
+            onPressed: () async {
+              _authentication.signOut();
+              final prefs = await SharedPreferences.getInstance();
+              prefs.remove("email");
+              prefs.remove("password");
+              if (!mounted) return;
+              Navigator.pop(context);
+            },
+          ),
+          title: SizedBox(
+            height: 300,
+            child: Image.asset("assets/lightforest_appbar_logo.png"),
+          ),
+        ),
         body: Column(
           children: [
+            const SizedBox(
+              height: 70,
+            ),
             TableCalendar(
               firstDay: DateTime.utc(2021, 10, 16),
               lastDay: DateTime.utc(2030, 3, 14),
               focusedDay: DateTime.now(),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                _authentication.signOut();
-                Navigator.pop(context);
-              },
-              child: Text("PREV"),
             ),
           ],
         ),
